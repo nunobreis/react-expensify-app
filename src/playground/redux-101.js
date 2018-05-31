8ad4f1447fe7c2d9d5364a51dc4552e6d1b864f3
nunobreis <nuno@catalpa.io>
2018-05-31 13:07:30 +0200

    Adds playground file with actions documentation

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,46 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      }
+    default:
+      return state;
+  }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Actions - objects that get sent to the store
+
+// INCREMENT CASE:
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+// RESET COUNTER:
+store.dispatch({
+  type: 'RESET'
+});
+
+// DECREMENT CASE:
+store.dispatch({
+  type: 'DECREMENT'
+});
