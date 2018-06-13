85a0bfd5a257e6c34ddaf774e2530a574f1fa4a7
nunobreis <nuno@catalpa.io>
2018-06-13 17:50:14 +0200

    adds the reducers tensting

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,25 +1,6 @@
 import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
-
-const expenses = [{
-  id: '1',
-  description: 'Gum',
-  note: '',
-  amount: 195,
-  createdAt: 0
-}, {
-  id: '2',
-  description: 'Rent',
-  note: '',
-  amount: 195000,
-  createdAt: moment(0).subtract(4, 'days').valueOf()
-}, {
-  id: '3',
-  description: 'Magazine',
-  note: '',
-  amount: 395,
-  createdAt: moment(0).add(4, 'days').valueOf()
-}];
+import expenses from "../fixtures/expenses";
 
 // Filter by text
 test('should filter by text value', () => {
